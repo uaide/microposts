0268261f10f29998dcc0e20477338701970bdfeb
ideyuki <yukigpt@gmail.com>
2015-12-10 19:18:38 +0000

    Fixed application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Microposts
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.generetors do |g|
+    config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
