981ebf1f18f7e7f41df6c3285a31f3b303010d98
ideyuki <yukigpt@gmail.com>
2015-12-19 03:53:52 +0000

    access error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:update, :edit]
+  before_action :set_user, only: [:update, :edit, :show]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -12,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Sample App!" + "「" + @user.name + "」"
       redirect_to @user
     else
       render 'new'
@@ -20,6 +19,12 @@ class UsersController < ApplicationController
   end
   
   def edit
+    if logged_in? && @user == current_user
+    else
+      flash[:warning] = "no access!"
+      redirect_to root_path
+      
+    end
   end
   
   def update
@@ -27,9 +32,11 @@ class UsersController < ApplicationController
       flash[:info] = "プロフィールを編集しました"
       redirect_to user_path   
     else
-      flash[:alert] = "プロフィールを編集しました"
+      flash[:danger] = "入力に誤りがあります。"
+      render 'edit'
     end
   end
+
     
   private
   
@@ -39,7 +46,7 @@ class UsersController < ApplicationController
   end
   
   def set_user
-    @user = current_user
+    @user = User.find(params[:id])
   end
   
 end
